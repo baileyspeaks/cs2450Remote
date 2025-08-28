5ca774bdf3bfb2082c46da8cf3f2356e911904cb
Bailey Speakman <baileyspeakman@Baileys-MacBook-Pro-2.local>
2025-08-28 11:11:16 -0600

    added the ability to get input from the user asking older or younger

--- age_guesser.py
+++ age_guesser.py
@@ -2,16 +2,26 @@ import random
 
 print("Hello. I am going to try and guess your age")
 name = input("What is your name? ")
+
+low = 1
+high = 100
 x = False
+
 while x == False:
-    age = random.randint(15, 30)
-    yesorno = input(f"Are you {age} years old? y or n? ")
+    age = random.randint(low, high)
+    yesorno = input(f"Are you {age} years old? y or n? ").lower()
     if yesorno == "y":
         print(f" {name} is {age} years old")
         x = True
-        quit
     elif yesorno == "n":
         print("Rats")
-        
+        oldoryoung = input(f"Older or Younger? ").lower()
+        if oldoryoung == "older":
+            low = age + 1
+        elif oldoryoung == "younger":
+            high = age - 1
+        else:
+            print("Please enter 'Older' or 'Younger'")
+         
     else:
-        yesorno = input(f"Please enter y or n")
+        print("Please enter y or n")
